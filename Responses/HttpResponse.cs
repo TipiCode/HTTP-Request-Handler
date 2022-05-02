961f74bffa98338e35f255531bd2bc3bdf70647d
Ed Mendoza <luisemendoza3@outlook.com>
2022-05-02 16:47:27 -0600

    Added remarks to the HttpResponse

--- Responses/HttpResponse.cs
+++ Responses/HttpResponse.cs
@@ -2,6 +2,9 @@
 
 namespace HttpRequestHandler.Responses
 {
+    /// <summary>
+    /// Class <c>HttpResponse</c> serves as a response for the <c>HttpRequestHandler</c> class.
+    /// </summary>
     public class HttpResponse
     {
         public HttpStatusCode StatusCode { get; set; }
